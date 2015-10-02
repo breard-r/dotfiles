161b548111eb301474f52e1f7a728b35a443d4e3
Rodolphe Breard <rodolphe@what.tf>
2015-10-02 14:21:54 +0200

    Adding the apache license.

--- .emacs.d/lisp/std_comment.el
+++ .emacs.d/lisp/std_comment.el
@@ -170,6 +170,20 @@
 	"Free Documentation License\".\n")
       )
 
+(setq license-apache
+      '("Licensed under the Apache License, Version 2.0 (the \"License\");\n"
+	"you may not use this file except in compliance with the License.\n"
+	"You may obtain a copy of the License at\n"
+	"\n"
+    "    http://www.apache.org/licenses/LICENSE-2.0\n"
+    "\n"
+    "Unless required by applicable law or agreed to in writing, software\n"
+    "distributed under the License is distributed on an \"AS IS\" BASIS,\n"
+    "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"
+    "See the License for the specific language governing permissions and\n"
+	"limitations under the License.\n")
+      )
+
 (setq license-bsd 
       '("Redistribution and use in source and binary forms, with or without\n"
 	"modification, are  permitted provided that the following conditions are\n"
@@ -346,7 +360,9 @@
     (if (string= license "lgpl")
 	(setq ltow license-lgpl))
     (if (string= license "fdl")
-	(setq ltow license-fdl))
+    (setq ltow license-fdl))
+    (if (string= license "apache")
+	(setq ltow license-apache))
     (if (string= license "bsd")
 	(setq ltow license-bsd))
     (if (string= license "isc")
