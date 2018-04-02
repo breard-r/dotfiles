1f86a61f96c48f7eea5c8305dd48de943fb03a5b
Rodolphe Breard <rodolphe@what.tf>
2018-04-02 14:28:54 +0200

    Add cargo's bin directory to the $PATH

--- .setpath.sh
+++ .setpath.sh
@@ -1,6 +1,11 @@
-BIN_PATH="$HOME/.local/bin"
-if [ -d "$BIN_PATH" ]; then
-    found=$(echo $PATH | tr ':' '\n' | grep -w "$BIN_PATH")
-    [ ! "$found" ] && export PATH="$BIN_PATH:$PATH"
-    true
-fi
+add_path() {
+    BIN_PATH="$1"
+    if [ -d "$BIN_PATH" ]; then
+        found=$(echo $PATH | tr ':' '\n' | grep -w "$BIN_PATH")
+        [ ! "$found" ] && export PATH="$BIN_PATH:$PATH"
+        true
+    fi
+}
+
+add_path "$HOME/.local/bin"
+add_path "$HOME/.cargo/bin"
